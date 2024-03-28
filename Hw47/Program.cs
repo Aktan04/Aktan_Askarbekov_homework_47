29dc8827cb145756f4a54b01af7d24fa55b815c4
Askarbekov Aktan <akn392204@gmail.com>
2024-03-28 14:36:53 +0600

    added code in Main and time txt

---
+++ Hw47/Program.cs
@@ -0,0 +1,9 @@
+﻿using Hw47;
+
+Employee employee = new Employee { Name = "Ivanov Ivan", Experience = 5 };
+MySerializer<Employee> employeeSerializer = new MySerializer<Employee>(employee);
+string serializedEmployee = employeeSerializer.Serialize();
+Console.WriteLine(serializedEmployee);
+   
+
+
